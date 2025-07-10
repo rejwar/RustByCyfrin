8321d0e70e39e7d41d867ce7394de93323cb4a9c
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-07-11 04:23:28 +0600

    Hi

--- Overflow/overflow.rs
+++ Overflow/overflow.rs
@@ -5,4 +5,10 @@ fn main() {
     X += 1;
 
     println!("u32 max: {} , x : {}" , u32::MAX, X);
+
+    let x = u32::Checked_add(u32::MAX , 1);
+    println!("Checked_add: {?}",X);
+
+    let x = u32::wrappig_add(u32::MAX,1);
+    println!("wrapping add : {:?}",X);
 }
